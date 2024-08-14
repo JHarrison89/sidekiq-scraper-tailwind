db4f4fc104300110dfe1ddede11160d51b9a691b
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2024-08-14 08:36:03 +0100

    Add authentication_zero

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+  resource :account, only: [:show]
+  resources :jobs
+
   mount Sidekiq::Web => '/sidekiq' # mount Sidekiq::Web in your Rails app
 
-  resources :jobs
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
