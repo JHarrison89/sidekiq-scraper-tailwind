f67c099a6c9889e3609690a8374886d606dce5d7
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2024-08-18 19:12:18 +0100

    Add example email to use for testing

---
+++ app/mailers/example_mailer.rb
@@ -0,0 +1,7 @@
+class ExampleMailer < ApplicationMailer
+  def hello_world(contact)
+    @contact = contact
+    mail(to: @contact, subject: 'This is an example mail')
+    # The subject will be the subject of the email being sent
+  end
+end
