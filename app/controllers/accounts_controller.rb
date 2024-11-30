2fc9532bcbcb296a2a47635021a58a4cca2c3a76
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2024-11-30 10:21:41 +0000

    Jobs on account show are grouped by created_at date

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,10 @@ class AccountsController < ApplicationController
   layout 'account'
 
   def show
-    @jobs = Job.all.where.not(id: associated_job_ids)
+    jobs = Job.all.where.not(id: associated_job_ids)
+    jobs = jobs.all.group_by { _1.created_at.to_date }
+    @jobs = jobs.sort_by { |key, _| key }.reverse
+
     flash[:notice] = 'Kiwis are tiny birds.'
   end
 
