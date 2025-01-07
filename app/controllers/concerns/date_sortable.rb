46090b0197f510aa5a6fafd50f5de80e4f110c44
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2025-01-07 13:15:00 +0000

    Remmove DateSortable concern
    No longer used, replaced by app/services/record_grouper.rb

--- app/controllers/concerns/date_sortable.rb
+++
@@ -1,8 +0,0 @@
-module DateSortable
-  extend ActiveSupport::Concern
-
-  def organize_by_date(jobs)
-    result = jobs.all.group_by { _1.created_at.to_date }
-    result.sort_by { |key, _| key }.reverse
-  end
-end
