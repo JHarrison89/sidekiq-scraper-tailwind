25f7918bab854d047872c2d0acfe94dd0b01163c
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2025-01-26 12:22:34 +0000

    Add new columns to Job table

---
+++ db/migrate/20250124155420_add_html_location_employer_to_job.rb
@@ -0,0 +1,7 @@
+class AddTextColumnForHtmlToJob < ActiveRecord::Migration[7.2]
+  def change
+    add_column :jobs, :employer, :string
+    add_column :jobs, :location, :string
+    add_column :jobs, :html_content, :text
+  end
+end
