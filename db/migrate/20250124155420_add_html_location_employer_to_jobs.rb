7a7d0c080f1de1f03c84494337b1e45a2f5dcd0e
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2025-01-27 21:24:48 +0000

    Rename migration to update Jobs table with employer and location columns

--- db/migrate/20250124155420_add_html_location_employer_to_jobs.rb
+++ db/migrate/20250124155420_add_html_location_employer_to_jobs.rb
@@ -1,4 +1,4 @@
-class AddTextColumnForHtmlToJob < ActiveRecord::Migration[7.2]
+class AddHtmlLocationEmployerToJobs < ActiveRecord::Migration[7.2]
   def change
     add_column :jobs, :employer, :string
     add_column :jobs, :location, :string
