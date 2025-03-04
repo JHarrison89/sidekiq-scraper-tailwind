70c7effc30876dd52dc8e292d1aca26e8e519ce8
Jeremaia Harrison <Jeremaia.harrison@gmail.com>
2025-03-04 17:59:27 +0000

    Refactor the job_list partial

--- app/helpers/logo_helper.rb
+++ app/helpers/logo_helper.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 module LogoHelper
-  def logo_helper(board:)
+  def board_logo_helper(board:)
     file_path = Rails.root.join("app/assets/images/logos/#{board.downcase}.jpg")
 
     if File.exist?(file_path)
       image_path("logos/#{board.downcase}.jpg")
+    else
+      image_path("logos/default.jpg")
+    end
+  end
+
+  def employer_logo_helper(employer:)
+    if employer.logo.attached?
+      rails_storage_redirect_path(employer.logo)
     else
       image_path("logos/default.jpg")
     end
